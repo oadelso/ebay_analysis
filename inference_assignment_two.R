22e8206bd2212bcee2183ec4abf7723f88d2e87d
oadelso <oseas.adelso@gmail.com>
2017-12-04 21:47:05 -0800

    Update inference_assignment_two.R

--- inference_assignment_two.R
+++ inference_assignment_two.R
@@ -10,27 +10,36 @@ df_test = select(df_test, -(boolean))
 chosen_model = lm(formula = price ~ age + kilometer + powerPS + vehicleType + brand + brand:vehicleType +
                   age:vehicleType + I(age^2):vehicleType, data = df_test)
 
-rmse_test = sqrt(mean(predict(chosen_model, df_test) - df_test$price)**2))
+rmse_test = sqrt(mean(predict(chosen_model, df_test) - df_test$price)**2)
 
 #start with inference
 
 #sample from the data
 #number of simulations
-covariates = data.frame(summary(chosen_model)$coefficients[,1])
+coefficients = data.frame(summary(chosen_model)$coefficients[,1])
 #get the number of covariates
-r = dim(covariates)[1]
-n = 25
+#r = dim(coefficients)[1]
+#focus on first four for now
+r = 4
+n = 100
 inference = matrix(rep(0, n*r), nrow = r, ncol = n)
 #label each row in inference 
-rownames(inference)  = rownames(covariates)
+rownames(inference)  = rownames(covariates)[1:4]
 for (i in 1:n){
-  input_index = sample(1:dim(df_test)[1], replace = TRUE)
+  #choose with replacement indices from the df_test
+  input_index = sample(1:dim(df_test)[1], dim(df_test)[1], replace = TRUE)
+  #build a new df_test from the indices above
   df_new_test = df_test[input_index, ]
+  #train the new model
   chosen_model = lm(formula = price ~ age + kilometer + powerPS + vehicleType + brand + brand:vehicleType +
-                      age:vehicleType + I(age^2):vehicleType, data = df_test)
+                      age:vehicleType + I(age^2):vehicleType, data = df_new_test)
   
-  new_covariates = matrix(summary(chosen_model)$coefficients[,1])
-  inference[, i] = new_covariates
+  #get the new value for the coefficient of the covariates
+  new_coefficients = matrix( summary( chosen_model )$coefficients[1:4,1] )
+  inference[ , i ] = new_coefficients
   
 }
+
+#view histogram for the values of the intercept
+hist(inference[, 1])
   
